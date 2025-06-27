a86a6c07c3fdccc38210015b7cc3db68d3ceac20
ummetarin <ummehomairatarin@gmail.com>
2025-06-27 22:51:57 +0600

    Array craetion method 3

--- My_Numpy.py
+++ My_Numpy.py
@@ -31,3 +31,14 @@ np.ones((2,3))
 #Identity matrix
 np.eye(3)
 #There will be only one input
+
+
+#arange matrix
+np.arange(1,10)
+np.arange(1,10,2)
+np.arange(0,10,2)
+
+
+#linspace->(1 theke 2 porjonto 5 ta value)
+np.linspace(1,2,5)
+np.linspace(1,2,1)
